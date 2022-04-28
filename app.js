82e23094efc040c7fc7334ad301f71e6d1c0bba4
yingmin99 <104242457+yingmin99@users.noreply.github.com>
2022-04-28 09:45:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3001;
 app.set("port", port);
 
 app.get("/", (req, res) => {
-  res.send("알뜰리에 홈페이지");
+  res.send("알뜰리에 홈페이지888");
 });
 
 app.listen(port, () => console.log("Listening on", port));
